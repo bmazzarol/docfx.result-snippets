b3e5db8034b355d2e8a5b2368503a33960a6bc4f
Ben Mazzarol <bmazzarol@gmail.com>
2023-06-04 17:13:33 +0800

    docs: added github link and search [ci]

---
+++ Docfx.ResultSnippets.Docs/template/public/main.js
@@ -0,0 +1,9 @@
+﻿export default {
+    iconLinks: [
+        {
+            icon: 'github',
+            href: 'https://github.com/bmazzarol/docfx.result-snippets',
+            title: 'GitHub'
+        }
+    ]
+}
